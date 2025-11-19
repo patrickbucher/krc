ded2b6b222c2049f87b181373de5a32ac0c36fef
Patrick Bucher <patrick.bucher@mailbox.org>
2025-11-19 22:55:59 +0100

    chapter 1, exercise 3; Makefile

---
+++ ch1/ex03.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4) {
+        printf("usage: %s LOWER UPPER STEP\n", argv[0]);
+        return 1;
+    }
+
+    int lower = atoi(argv[1]);
+    int upper = atoi(argv[2]);
+    int step = atoi(argv[3]);
+
+    if (lower >= upper) {
+        puts("upper must be greater than lower");
+        return 1;
+    }
+    if (step <= 0) {
+        puts("step must be positive");
+        return 1;
+    }
+
+    printf("%6s\t%6s\n", "F", "C");
+    while (lower <= upper) {
+        float fahr = (float) lower;
+        float cel = 5 * (fahr - 32) / 9;
+        printf("%6.2f\t%6.2f\n", fahr, cel);
+        lower += step;
+    }
+
+    return 0;
+}
