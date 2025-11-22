f9e600082d82e9cdf88ea2b84d1e47f868aac85d
Patrick Bucher <patrick.bucher@mailbox.org>
2025-11-22 11:53:52 +0100

    chapter 1, exercise 11

---
+++ ch1/ex11.c
@@ -0,0 +1,30 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+// The program's behaviour with single line inputs is questionable.
+//
+// $ echo -n 'This is a test' | ./ex11
+// 0 4 14
+
+int main(int argc, char *argv[])
+{
+    _Bool in = false;
+    char c = 0;
+    int nc = 0, nw = 0, nl = 0;
+
+    while ((c = getchar()) != EOF) {
+        nc++;
+        if (c == '\n') {
+            nl++;
+        }
+        if (c == ' ' || c == '\t' || c == '\n') {
+            in = false;
+        } else if (!in) {
+            in = true;
+            nw++;
+        }
+    }
+    printf("%d %d %d\n", nl, nw, nc);
+
+    return 0;
+}
