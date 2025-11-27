00612bded60eb78abc73df57018c0d9dcde3bf05
Patrick Bucher <patrick.bucher@mailbox.org>
2025-11-27 17:56:32 +0100

    chapter 1, exercise 17

---
+++ ch1/ex17.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define TRESHOLD 80
+#define BUFFERING 1
+#define PRINTING 2
+
+int main(int argc, char *argv[])
+{
+    char c, buf[TRESHOLD + 1];
+    int mode = BUFFERING, i = 0;
+
+    while ((c = getchar()) != EOF) {
+        if (i < TRESHOLD) {
+            buf[i++] = c;
+        } else if (mode == BUFFERING) {
+            mode = PRINTING;
+            buf[TRESHOLD] = '\0';
+            printf("%s", buf);
+        }
+        if (mode == PRINTING) {
+            putchar(c);
+        }
+        if (c == '\n') {
+            i = 0;
+            mode = BUFFERING;
+        }
+    }
+
+    return 0;
+}
