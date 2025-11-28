da2957e91ad6cc37e61f176db40e1fc05bb8f91a
Patrick Bucher <patrick.bucher@mailbox.org>
2025-11-28 07:01:16 +0100

    chapter 1, exercise 18

---
+++ ch1/ex18.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define BUFSIZE 1024
+
+_Bool is_whitespace(char c)
+{
+    return c == ' ' || c == '\t';
+}
+
+int main(void)
+{
+    char c, buf[BUFSIZE];
+    int i, j;
+
+    i = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == '\n') {
+            for (j = i; j >= 0 && is_whitespace(buf[j]); j--) {
+                buf[j] = '\0';
+            }
+            if (i > 0) {
+                printf("%s\n", buf);
+                i = 0;
+                while (j >= 0) {
+                    buf[j--] = '\0';
+                }
+            }
+        } else {
+            buf[i++] = c;
+        }
+    }
+
+    return 0;
+}
