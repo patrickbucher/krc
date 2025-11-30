4f86dd97e1150a5474e02b991ef4580205e5ca4f
Patrick Bucher <patrick.bucher@mailbox.org>
2025-11-30 08:34:12 +0100

    chapter 1, exercise 20

---
+++ ch1/ex20.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define TAB_AT 4
+
+int main(void)
+{
+    char c;
+    int pos, nextTabAt;
+
+    pos = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == '\n') {
+            pos = 0;
+            putchar(c);
+        } else if (c == '\t') {
+            while (pos == 0 || pos % TAB_AT != 0) {
+                putchar(' ');
+                pos++;
+            }
+        } else {
+            putchar(c);
+            pos++;
+        }
+    }
+
+    return 0;
+}
