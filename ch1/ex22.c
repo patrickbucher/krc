44080395b77634ccac2f743c801e22ca9c5e7148
Patrick Bucher <patrick.bucher@mailbox.org>
2025-12-07 08:59:43 +0100

    [WIP] chapter 1, exercise 22

--- ch1/ex22.c
+++ ch1/ex22.c
@@ -4,20 +4,41 @@
 
 int main(void)
 {
-    char line[LINE_LENGTH];
-    int last_blank_at - 1, i = 0, c;
+    char line[LINE_LENGTH + 1], c;
+    int last_blank_at = -1, last_non_blank_at = -1, i = 0;
 
     while ((c = getchar()) != EOF) {
-        line[i++] = c;
+        if (i < LINE_LENGTH) {
+            line[i++] = c;
+        } else if (last_blank_at != -1) {
+            for (int j = last_blank_at; j >= 0; j--) {
+                if (line[j] != '\t' && line[j] != ' ') {
+                    last_non_blank_at = j;
+                    break;
+                }
+            }
+            line[last_non_blank_at + 1] = '\0';
+            puts(line);
+            for (int r = last_non_blank_at + 1, w = 0; r < i; r++) {
+                line[w++] = line[r++];
+                i = w;
+                if (line[i] == '\t' || line[i] == ' ') {
+                    last_blank_at = i;
+                }
+            }
+        } else {
+            // TODO: hard break, implement later
+        }
         if (c == '\n') {
-            // TODO: flush
+            line[i] = '\0';
+            puts(line);
+            i = 0;
+            last_blank_at = -1;
         } else if (c == '\t' || c == ' ') {
             last_blank_at = i;
         }
-        if (w >= LINE_LENGTH) {
-            // TODO output line[0..last_blank_at], conserve line[last_blank_at..i]
-        }
     }
+    puts(line);
 
     return 0;
 }
---
+++ ch1/text.md
@@ -0,0 +1,2 @@
+This is some text with rather long lines. As usual in Markdown, the paragraphs are separated by blank lines. But one paragraph is a line, so there's no line break after 72 or so characters.
+
