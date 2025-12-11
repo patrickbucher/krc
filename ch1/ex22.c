516858afafd9052f70798dedaab0a91bb66081a0
Patrick Bucher <patrick.bucher@mailbox.org>
2025-12-11 12:48:00 +0100

    chapter 1, exercise 22

--- ch1/ex22.c
+++ ch1/ex22.c
@@ -1,26 +1,46 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define LINE_LENGTH 24
+#define LINE_LENGTH 12
 
 int main(void)
 {
     char line[LINE_LENGTH + 1]; // 0-terminated
     char c;
-    int i = 0, bp = 0;
+    int i = 0, j = 0, r = 0, w = 0, bp = 0;
 
     while ((c = getchar()) != EOF) {
-        if (i < LINE_LENGTH) {
-            line[i] = c;
-        } else {
-            line[bp] = '\0';
+        if (c == '\n') {
+            line[i] = '\0';
             puts(line);
-            i = -1;
+            i = 0;
+        } else if (c == ' ') {
+            if (i < LINE_LENGTH) {
+                line[i] = c;
+                if (i > 0 && line[i - 1] != ' ') {
+                    bp = i;
+                }
+                i++;
+            } else {
+                line[i] = '\0';
+                puts(line);
+                i = 0;
+            }
+        } else if (i < LINE_LENGTH) {
+            line[i++] = c;
+            line[i] = '\0';
+        } else {
+            for (j = 0; j < bp; j++) {
+                putchar(line[j]);
+            }
+            putchar('\n');
+            for (r = bp + 1, w = 0; line[r] != '\0' && r < i;) {
+                line[w++] = line[r++];
+            }
+            line[w++] = c;
+            line[w] = '\0';
+            i = w;
         }
-        if (i > 1 && c == ' ' && line[i - 1] != ' ') {
-            bp = i;
-        }
-        i++;
     }
 
     return 0;
--- ch1/text.md
+++
@@ -1,2 +0,0 @@
-This is some text with rather long lines. As usual in Markdown, the paragraphs are separated by blank lines. But one paragraph is a line, so there's no line break after 72 or so characters.
-
