e49a84cf34c46d16174d17e4728ca1e516a131c1
Patrick Bucher <patrick.bucher@mailbox.org>
2025-12-21 14:44:48 +0100

    chapter 2, exercise 1 (float/double calculations pending)

--- ch2/ex01.c
+++ ch2/ex01.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 unsigned long upow(int, int);
+double fpow(float, float);
 
 int main(void)
 {
@@ -30,18 +31,31 @@ int main(void)
     long_bits = long_bytes * 8 - 1;
 
     printf("signed char:\t[%d,%d]\n",
-           -(upow(2, char_bits)), upow(2, char_bits) - 1);
-    printf("unsigned char:\t[%d,%u]\n", 0, upow(2, char_bits + 1) - 1);
-    printf("signed short:\t[%d,%d]\n",
-           (short) -(upow(2, short_bits)),
+           (char) -(upow(2, char_bits)), (char) upow(2, char_bits) - 1);
+    printf("unsigned char:\t[%d,%u]\n", 0,
+           (unsigned char) upow(2, char_bits + 1) - 1);
+    printf("signed short:\t[%d,%d]\n", (short) -(upow(2, short_bits)),
            (short) upow(2, short_bits) - 1);
-    printf("unsigned short:\t[%d,%u]\n", 0, upow(2, short_bits + 1) - 1);
-    printf("signed int:\t[%d,%d]\n",
-           (int) -(upow(2, int_bits)), (int) upow(2, int_bits) - 1);
+    printf("unsigned short:\t[%d,%u]\n", 0,
+           (unsigned short) upow(2, short_bits + 1) - 1);
+    printf("signed int:\t[%d,%d]\n", (int) -(upow(2, int_bits)),
+           (int) upow(2, int_bits) - 1);
     printf("unsigned int:\t[%d,%u]\n", 0, 0 - 1);
     printf("signed long:\t[%ld,%ld]\n",
            (long) -(upow(2, long_bits)), (long) upow(2, long_bits) - 1);
     printf("unsigned long:\t[%d,%lu]\n", 0, (unsigned long) 0 - 1);
+
+    /* floating point constants */
+    printf("float:\t\t[%g,%g]\n", FLT_MIN, FLT_MAX);
+    printf("double:\t\t[%g,%g]\n", DBL_MIN, DBL_MAX);
+
+    /* floating point calculations */
+    /* FIXME: overflow */
+    printf("float:\t\t[%g,%g]\n", -fpow(FLT_RADIX, FLT_MAX_EXP),
+           fpow(FLT_RADIX, FLT_MAX_EXP));
+    /* FIXME: underflow, overflow */
+    printf("double:\t\t[%g,%g]\n", -fpow(FLT_RADIX, DBL_MAX_EXP),
+           fpow(FLT_RADIX, DBL_MAX_EXP));
 
     return 0;
 }
@@ -50,6 +64,16 @@ unsigned long upow(int b, int e)
 {
     int i;
     unsigned long x = b;
+    for (i = 1; i < e; i++) {
+        x *= b;
+    }
+    return x;
+}
+
+double fpow(float b, float e)
+{
+    int i;
+    double x = b;
     for (i = 1; i < e; i++) {
         x *= b;
     }
