67fa5a292d9c6a2602f8b0d648e6cc1af79c67a9
Patrick Bucher <patrick.bucher@mailbox.org>
2025-12-28 12:02:17 +0100

    chapter 2, exercise 7

--- ch2/ex07.c
+++ ch2/ex07.c
@@ -1,7 +1,8 @@
-#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+unsigned int length(unsigned int);
+unsigned int ones(unsigned int);
 unsigned int invert(unsigned int, unsigned int, unsigned int);
 
 int main(int argc, char *argv[])
@@ -17,23 +18,34 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-unsigned int invert(unsigned int x, unsigned int p, unsigned int n)
+unsigned int length(unsigned int x)
 {
-    unsigned int shift, left, swap, right;
+    unsigned int n;
 
-    shift = p - n + 1;
-    left = x >> (p + 1) << (p + 1);
-    swap = ((x >> shift) & ~(~0 << n));
-    right = ~(~0 << shift) & x;
+    for (n = 0; x > 0; n++) {
+        x >>= 1;
+    }
 
-    return (left & ~swap) | right;
+    return n;
 }
 
-/* TODO: invert(x=1000'0001, p=5, n=4)
-   1) create a mask of 1 bits of size n: 1111
-   2) shift the mask to the left to fit p: 111100
-   3) store the current bits using x & 111100 = 0000'0000
-   4) invert those bits = 1111'1111
-   5) get the new bits back using 1111'1111 & 111100 = 0011'1100
-   6) or the left and right part: 1000'0000 | 0011'1100 | 0000'0001 = 10111101
-*/
+unsigned int ones(unsigned int n)
+{
+    return ~((~0 >> n) << n);
+}
+
+unsigned int invert(unsigned int x, unsigned int p, unsigned int n)
+{
+    unsigned int shift, left, right, mask, state, inverted;
+
+    shift = p - n + 1;
+    left = (x >> (p + 1)) << (p + 1);
+    right = x & ones(shift);
+
+    mask = ones(n) << shift;
+    state = x & mask;
+    state = ~state & ones(p + 1);
+    inverted = state & mask;
+
+    return left | inverted | right;
+}
--- ch2/ex07_test.sh
+++ ch2/ex07_test.sh
@@ -4,10 +4,18 @@ set -euo pipefail
 
 make ex07
 
-# invert(11111111, 5, 3) = 11000111
-# invert(255, 5, 3) = 199
-test $(./ex07 255 5 3) -eq 199 || echo "err: invert(255, 5, 3)"
+# invert(1010, 2, 2) == 1100
+# invert(10, 2, 2) == 12
+test $(./ex07 10 2 2) -eq 12 || echo "err: invert(10, 2, 2)"
+
+# invert(10101010, 6, 7) == 11010101
+# invert(170, 6, 7) == 213
+test $(./ex07 170 6 7) -eq 213 || echo "err: invert(170, 6, 7)"
 
 # invert(10000001, 5, 4) = 10111101
 # invert(129, 5, 4) = 189
 test $(./ex07 129 5 4) -eq 189 || echo "err: invert(129, 5, 4)"
+
+# invert(11111111, 5, 3) = 11000111
+# invert(255, 5, 3) = 199
+test $(./ex07 255 5 3) -eq 199 || echo "err: invert(255, 5, 3)"
