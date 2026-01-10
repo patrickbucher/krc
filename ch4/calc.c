d35445dc3a5b75eb1872fbbd44a70e19e11f9fd9
Patrick Bucher <patrick.bucher@mailbox.org>
2026-01-10 11:34:59 +0100

    implemented "vanilla" calculator (i/o instead of arguments based) for further exercises

---
+++ ch4/calc.c
@@ -0,0 +1,112 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STACK_SIZE 1024
+#define BUFSIZE 100
+#define MAXOP 100
+#define NUMBER 0
+
+int getch(void);
+void ungetch(int);
+int getop(char[]);
+void push(double);
+double pop(void);
+
+double stack[STACK_SIZE];
+int sp = 0;
+char buf[BUFSIZE];
+int bufp = 0;
+
+int main(void)
+{
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() + pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if (op2 != 0.0) {
+                    push(pop() / op2);
+                } else {
+                    printf("error: zero divisor\n");
+                }
+                break;
+            case '\n':
+                printf("\t%.8g\n", pop());
+                break;
+            default:
+                printf("error: unknown command %s\n", s);
+                break;
+        }
+    }
+
+    return 0;
+}
+
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+    if (bufp >= BUFSIZE) {
+        fprintf(stderr, "ungetch: too many characters\n");
+        exit(1);
+    } else {
+        buf[bufp++] = c;
+    }
+}
+
+int getop(char s[])
+{
+    int i, c;
+
+    while ((s[0] = c = getch()) == ' ' || c == '\t');
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.') {
+        return c;               /* not a number */
+    }
+    i = 0;
+    if (isdigit(c)) {
+        while (isdigit(s[++i] = c = getch()));  /* collect integer part */
+        if (c == '.') {
+            while (isdigit(s[++i] = c = getch()));      /* collect fraction part */
+        }
+    }
+    s[i] = '\0';
+    if (c != EOF) {
+        ungetch(c);
+    }
+    return NUMBER;
+}
+
+void push(double d)
+{
+    stack[sp++] = d;
+}
+
+double pop(void)
+{
+    if (sp) {
+        return stack[--sp];
+    }
+    fprintf(stderr, "pop on empty stack");
+    exit(1);
+}
